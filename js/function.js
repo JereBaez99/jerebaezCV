912ee7a8819c8f986df32efb18503a948626fc88
Jeremias Baez <jerebaez99@gmail.com>
2023-02-02 01:41:13 -0300

    animacion-elementos

--- js/function.js
+++ js/function.js
@@ -2,6 +2,9 @@ const box_left_1 = document.getElementById("box-left-1");
 const box_left_2 = document.getElementById("box-left-2");
 const box_right_1 = document.getElementById("box-right-1");
 const box_right_2 = document.getElementById("box-right-2");
+const card_img_1 = document.getElementById("card-img-1");
+const card_img_2 = document.getElementById("card-img-2");
+const card_img_3 = document.getElementById("card-img-3");
 
 const cargarBox = (entradas, observador) => {
   entradas.forEach((entrada) => {
@@ -21,6 +24,13 @@ const cargarBox = (entradas, observador) => {
         entrada.target.classList.add("box-right-visible");
         // console.log("box-right");
       }
+      if (
+        entrada.target.id == "card-img-1" ||
+        entrada.target.id == "card-img-2" ||
+        entrada.target.id == "card-img-3"
+      ) {
+        entrada.target.classList.add("card-img-visible");
+      }
     }
   });
 };
@@ -31,12 +41,16 @@ const observador = new IntersectionObserver(cargarBox, {
   threshold: 0.5,
 });
 
-
-
-
-
+const observador_img = new IntersectionObserver(cargarBox, {
+root:null,
+rootMargin: "0px 0px 0px 0px",
+threshold: 1,
+});
 
 observador.observe(box_left_1);
 observador.observe(box_left_2);
 observador.observe(box_right_1);
 observador.observe(box_right_2);
+observador.observe(card_img_1);
+observador.observe(card_img_2);
+observador.observe(card_img_3);
